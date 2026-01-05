d615fccfda30f5b6d04df17fb0e42f98ad3af3f0
vladimir.solovev@1win.pro <vladimir.solovev@1win.pro>
2026-01-05 13:54:19 +0300

    feat: move task board to separate page with button in ActivityTracker

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -6,7 +6,6 @@ import { Header } from "../Header";
 import { ActivityTracker } from "../ActivityTracker";
 import { TestResults } from "../TestResults";
 import { CompletedMaterials } from "../CompletedMaterials";
-import { TaskBoard } from "../components/TaskBoard";
 import { MotivationalQuotes } from "../MotivationalQuotes";
 import Link from "next/link";
 import { useMemo } from "react";
@@ -119,8 +118,6 @@ export default function ProfilePage() {
             <UserInfoWithTopics userId={user.id} userName={user.name} userEmail={user.email} />
 
             <ActivityTracker userId={user.id} />
-
-            <TaskBoard userId={user.id} />
 
             <CompletedMaterials userId={user.id} />
 
